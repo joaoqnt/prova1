67eb16ab31fc05f89e2bdef863be0e9b9ac6931f
Joao Vitor Quintanilha <118181561+joaoqnt@users.noreply.github.com>
2024-08-01 19:46:25 -0300

    segundo  commit

--- src/main/java/br/edu/iftm/prova/model/Rastreamento.java
+++ src/main/java/br/edu/iftm/prova/model/Rastreamento.java
@@ -2,12 +2,22 @@ package br.edu.iftm.prova.model;
 
 import java.util.Date;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class Rastreamento {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
